5d6df3dfd690665ce7ba3281dbed12579b7c14b5
Jesusleonelbracamonte77 <100220249+Jesusleonelbracamonte77@users.noreply.github.com>
2022-03-14 08:19:20 -0400

    Add files via upload

---
+++ Jesus_Bracamonte_teleprgm1.py
@@ -0,0 +1,13 @@
+#Grupo 2
+nombre = "Jesús Leonel Bracamonte Martinez"
+
+print("hola Prog1, soy:",nombre)
+
+"""
+Estoy con la broma si es asi que lo quiere o si no lo
+voy hacer de esta forma
+"""
+
+nombre = input("Introduzca su nombre completo:")
+
+print("Hola Prog1, soy",format(nombre))
